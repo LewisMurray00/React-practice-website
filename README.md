32734aca1ef0d1bc9b28e68aa5e2fd3ecc605bcd
Lewis Murray <lewisdmurray00@gmail.com>
2022-01-24 17:52:27 +0000

    Added Font Awesome to my project as a means to get icons - Added a div container for the responsive 'hamburger' as well as a function that changes the icon on click

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 - <BrowserRouter> uses regular URL paths, so web server serves the same page at all URLs that are managed client side
 - When a <Switch> (V5) or <Routes> (V6+) is rendered it searches through its children elements to find one which path matches the current URL
 - <Route> are the children elements of the <Switch/Routes>
+
+## Font-Awesome
+
+- Used this as a way to get the icons for a hamburger bar
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -2,6 +2,13 @@ import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
+
+    // Setting state for the 'click' hamburger function
+    const [click, setClick] = useState(false);
+
+    // Create a function for the click on the hamburger function and sets it to the opposite of what click is 
+    const handleClick = () => setClick(!click);
+
   return (
     <>
      <nav className="navbar">
@@ -10,6 +17,12 @@ function Navbar() {
             <Link to="/" className="navbar-logo">
                 JW Constructions
             </Link>
+            
+            {/* Div container for the hamburger responsive menu */}
+            <div className="menu-icon" onClick={handleClick} >
+                {/* creates a state where if clicked it will turn into the 'x' icon but any other case it will remain the hamburger icon */}
+                <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+            </div>
         </div>
      </nav>
         
