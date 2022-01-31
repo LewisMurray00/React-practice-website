60e0e2b727aa13d680bc02610aaec60cea0d1e78
Lewis Murray <lewisdmurray00@gmail.com>
2022-01-31 18:48:11 +0000

    Added a HeroSection and began editing it

---
+++ src/Components/Hero/HeroSection.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HeroSection() {
+  return (
+    <div className="hero-container">
+        <h1>Welcome to JW Construction LTD</h1>
+        <p>Your vision, is our mission.</p>
+    </div>
+  );
+}
+
+export default HeroSection;
