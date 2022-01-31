2f7755b20d454c73288ca71498f137d93e4c5e91
Lewis Murray <lewisdmurray00@gmail.com>
2022-01-31 19:08:05 +0000

    Created a hero.js page and styled it- added a pages folder to the components tab

--- src/Components/Hero/HeroSection.js
+++ src/Components/Hero/HeroSection.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import 'C:\Users\Lewis\Documents\coding-projects\react-practice-website\src\App.js'
+import { Button } from '../Button/Button'
+import './HeroSection.css'
 
 function HeroSection() {
   return (
     <div className="hero-container">
         <h1>Welcome to JW Construction LTD</h1>
         <p>Your vision, is our mission.</p>
+        <div className="hero-btns">
+            <Button 
+            className="btns"
+            buttonStyle="btn--outline" 
+            buttonSize="btn--large"
+            >
+                Begin
+            </Button>
+            <Button 
+            className="btns"
+            buttonStyle="btn--primary" 
+            buttonSize="btn--large"
+            >
+                See video <i className="far fa-play-circle"/>
+            </Button>
+        </div>
     </div>
   );
 }
