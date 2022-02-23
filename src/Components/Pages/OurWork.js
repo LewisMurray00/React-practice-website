5696d3c4a13bec4b1ef7d440e4b407eaafa1d454
Lewis Murray <lewisdmurray00@gmail.com>
2022-02-23 22:08:20 +0000

    Made the slider responsive, still some more small changes to do

--- src/Components/Pages/OurWork.js
+++ src/Components/Pages/OurWork.js
@@ -3,12 +3,17 @@ import Footer from '../Footer/Footer';
 import OurWorkHero from '../OurWorkHero/OurWorkHero';
 import ImageSlider from '../Slider/ImageSlider';
 import { SliderData } from '../Slider/SliderData';
+import './PagesCss/OurWork.css'
 
 function OurWork() {
   return (
     <>
         <OurWorkHero />
-        <ImageSlider slides={SliderData}/>
+        <div className="landscaping-container">
+          <ImageSlider slides={SliderData}/>
+          <h1 className="landscaping-title">Landscaping</h1>
+          <p className="landscaping-info">Landscaping is fun</p>
+        </div>
         <Footer /> 
     </>
   );
