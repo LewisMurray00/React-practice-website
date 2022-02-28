fd8923d8086aa5e694f39c1f6ad0d4c2cb52d93b
Lewis Murray <lewisdmurray00@gmail.com>
2022-02-28 23:08:53 +0000

    Styled some CSS more for the OurWork.js file

--- src/Components/Pages/OurWork.js
+++ src/Components/Pages/OurWork.js
@@ -11,8 +11,9 @@ function OurWork() {
         <OurWorkHero />
         <div className="landscaping-container">
           <ImageSlider slides={SliderData}/>
-          <h1 className="landscaping-title">Landscaping</h1>
-          <p className="landscaping-info">Landscaping is fun</p>
+          <div className="landscaping-text">
+            <h1 className="landscaping-title">Landscaping</h1>
+          </div>  
         </div>
         <Footer /> 
     </>
