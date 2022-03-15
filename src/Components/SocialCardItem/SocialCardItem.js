37c7325705cd353a677cf4eb4e84ea8536481a75
Lewis Murray <lewisdmurray00@gmail.com>
2022-03-15 21:12:58 +0000

    Started creating the cards for the contact us section

---
+++ src/Components/SocialCardItem/SocialCardItem.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './SocialCardItem.css'
+
+function SocialCardItem(props) {
+  return (
+      <>
+        <li className="social__cards__item">
+            <Link to={props.path} className="social__cards__item__link">
+                <figure className="social__cards__item__pic-wrap">
+                    <img src={props.src} alt="Socials" className="social__cards__item__img" />
+                </figure>
+                <div>
+                    
+                </div>
+            </Link>
+
+        </li>
+      </>
+  )
+}
+
+export default SocialCardItem;
