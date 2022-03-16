47fca3053d1e9c14c044d1026541cc4a032f478f
Lewis Murray <lewisdmurray00@gmail.com>
2022-03-16 19:59:06 +0000

    Cards now showing on page, slight css issues

--- src/Components/Pages/ContactUs.js
+++ src/Components/Pages/ContactUs.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Footer from '../Footer/Footer';
+import SocialCards from '../SocialCards/SocialCards';
 
 function ContactUs() {
   return (
@@ -7,9 +8,7 @@ function ContactUs() {
         <div className="contactus-container">
             <h1>Contact Us</h1>
         </div>
-        <div className="social-media">
-
-        </div>
+        <SocialCards />
         <Footer />  
     </>
   );
