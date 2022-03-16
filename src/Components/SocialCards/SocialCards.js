292f6cfa905fac06ac7146fba9ac7ab63e6ceaf2
Lewis Murray <lewisdmurray00@gmail.com>
2022-03-16 19:49:54 +0000

    Created two new cards

--- src/Components/SocialCards/SocialCards.js
+++ src/Components/SocialCards/SocialCards.js
@@ -4,9 +4,27 @@ import './SocialCards.css'
 
 function SocialCards() {
   return (
-      <>
-
-      </>
+      <div className="socialCards">
+        <h1>Get in touch with us!</h1>
+        <div className="socialCards__container">
+          <div className="socialCards__wrapper">
+            <ul className="socialCards__items">
+              <SocialCardItem 
+                src="Images/0x0.png"
+                text="Check out our instagram here!"
+                label="Instagram"
+                path="/contact-us"
+              />
+              <SocialCardItem
+                src="Images/0x0.png"
+                text="Check out our facebook here"
+                label="Facebook"
+                path="/contact-us"
+              />
+            </ul>
+          </div>
+        </div>
+      </div>
   );
 }
 
