520f08edd8e08dc03efc21ad16d81872954b2b45
Lewis Murray <lewisdmurray00@gmail.com>
2022-03-21 20:05:34 +0000

    Made the footer more appropriate for the webpage aim

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -6,24 +6,6 @@ import { Link } from 'react-router-dom';
 function Footer() {
   return (
       <div className='footer-container'>
-        <div className='footer-links'>
-            <div className='footer-link-wrapper'>
-                <div className='footer-link-items'>
-                    <h2>Pages</h2>
-                    <Link to='/'>Home</Link>
-                    <Link to='our-work'>Our work</Link>
-                    <Link to='reviews'>Reviews</Link>
-                    <Link to='contact-us'>Contact us</Link>
-                </div>
-                <div className='footer-link-items'>
-                    <h2>Contact us</h2>
-                    <Link to='/'>Contact</Link>
-                    <Link to='our-work'>Support</Link>
-                    <Link to='reviews'>Destinations</Link>
-                    <Link to='contact-us'>Sponsorships</Link>
-                </div>
-            </div>
-        </div>
         <section className="social-media">
             <div className='social-media-wrap'>
                 <small className='website-rights'>JWConstructionLTD 2022</small>
