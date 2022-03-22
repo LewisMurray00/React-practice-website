2d3f9a1d02cde38003f7d93a2eda63d095c27a61
Lewis Murray <lewisdmurray00@gmail.com>
2022-03-22 20:13:49 +0000

    Started the review page, adding a slide show

---
+++ src/Components/AllSliders/ReviewSliderData.js
@@ -0,0 +1,8 @@
+export const ReviewSliderData = [
+    {
+        image:'Images/image3.jpeg'
+    },
+    {
+        image:'Images/0x0.png'
+    }
+]
--- src/Components/Pages/Reviews.js
+++ src/Components/Pages/Reviews.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import Footer from '../Footer/Footer';
+import ImageSlider from '../Slider/ImageSlider'
+import {ReviewSliderData} from '../AllSliders/ReviewSliderData'
+import '../Pages/PagesCss/Reviews.css'
 
 function Reviews() {
   return (
     <>
     <div>
         <h1>Reviews</h1>
-    </div>  
+    </div>
+    <div className="review-container">
+          <ImageSlider slides={ReviewSliderData} />
+          <div className="review-text">
+            <h1 className="review-title">Reviews</h1>
+          </div>
+        </div>  
     <Footer />
 </>
   );
