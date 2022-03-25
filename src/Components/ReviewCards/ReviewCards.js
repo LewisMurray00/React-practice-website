18afcea19d6d6cf0b8cd7f9b06bf9efd505dfe56
Lewis Murray <lewisdmurray00@gmail.com>
2022-03-25 19:20:09 +0000

    ReviewCards done

--- src/Components/ReviewCards/ReviewCards.js
+++ src/Components/ReviewCards/ReviewCards.js
@@ -4,8 +4,34 @@ import './ReviewCards.css'
 
 function ReviewCards() {
   return (
-    <div>ReviewCards</div>
-  )
+    <div className="reviewCards">
+        <h1>Check out our work!</h1>
+        <div className="review__cards__container">
+            <div className="review__cards__wrapper">
+                <ul className="review__cards__items">
+                    <ReviewCardItem 
+                        src="Images/0x0.jpeg"
+                        text="Check out a review from one of our customers"
+                        label="Review"
+                        path='/reviews'
+                    />
+                    <ReviewCardItem 
+                        src="Images/0x0.png"
+                        text="Check out a review from one of our customers!"
+                        label="Reviews here"
+                        path='/reviews'
+                    />
+                    <ReviewCardItem 
+                        src="Images/0x0.png"
+                        text="Check out a review from one of our customers!"
+                        label="Reviews here"
+                        path='/reviews'
+                    />
+                </ul>
+            </div>
+        </div>
+    </div>
+  );
 }
 
 export default ReviewCards
