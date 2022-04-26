fad7291ea48e7376c12d4be67e992d7f94b608c4
Lewis Murray <lewisdmurray00@gmail.com>
2022-04-26 22:05:27 +0100

    Styled the div container for the aboutHome sectionW

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -3,11 +3,13 @@ import '../../App.css'
 import Cards from '../Cards/Cards';
 import Footer from '../Footer/Footer';
 import HeroSection from '../Hero/HeroSection'
+import AboutHome from '../About(Home)/AboutHome'
 
 function Home(){
     return (
         <>
             <HeroSection />
+            <AboutHome />
             <Cards />
             <Footer />
         </>
