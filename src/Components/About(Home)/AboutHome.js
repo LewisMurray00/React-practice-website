0debe2ed0dd60c9c26ccd3553325ae48d2508c5e
Lewis Murray <lewisdmurray00@gmail.com>
2022-04-26 21:58:37 +0100

    Added a new AboutHome section

---
+++ src/Components/About(Home)/AboutHome.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './AboutHome.css'
+
+function AboutHome() {
+  return (
+    <div className="about-container">
+        <h1 className="about-title">Our story</h1>
+        <p className="about-textS">
+        Having been established in 2014, We at JW.Construction offer a bespoke solution to all of your building requirements. We are a one-stop compnany, that does everything using our trade specific employees and sub contractors.
+        </p>
+    </div>
+  )
+}
+
+export default AboutHome
