48f88bbcb82ecd15fdb7d22b877a9d31f6e98af2
Lewis Murray <lewisdmurray00@gmail.com>
2022-04-28 18:53:08 +0100

    styled the about section of the home page

--- src/Components/About(Home)/AboutHome.js
+++ src/Components/About(Home)/AboutHome.js
@@ -5,8 +5,9 @@ function AboutHome() {
   return (
     <div className="about-container">
         <h1 className="about-title">Our story</h1>
-        <p className="about-textS">
-        Having been established in 2014, We at JW.Construction offer a bespoke solution to all of your building requirements. We are a one-stop compnany, that does everything using our trade specific employees and sub contractors.
+        <p className="about-texts">
+        Since 2014, we at JW Construction have created a multi-disciplined team of professional tradesmen that are capable of achieving any tasks at hand and have truly transformed our clients dreams into reality.
+        <br /> Our enthusiasm, dedication, honesty and cleanliness have given us the reputation that we have today, which is proven in our reviews, a testament to the quality of our work.
         </p>
     </div>
   )
