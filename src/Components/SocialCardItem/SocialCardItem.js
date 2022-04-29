d469cc124080d531765a3fbc5863b0dd3b9b2068
Lewis Murray <lewisdmurray00@gmail.com>
2022-04-29 22:15:35 +0100

    Fixed the target blank to rel noopener

--- src/Components/SocialCardItem/SocialCardItem.js
+++ src/Components/SocialCardItem/SocialCardItem.js
@@ -5,7 +5,7 @@ function SocialCardItem(props) {
   return (
       <>
         <li className="social__cards__item">
-            <a href={props.path} target="_blank" className="social__cards__item__link">
+            <a href={props.path} rel="noopener" className="social__cards__item__link">
                 <figure className="social__cards__item__pic-wrap" data-category={props.label}>
                     <img src={props.src} alt="Socials" className="social__cards__item__img" />
                 </figure>
