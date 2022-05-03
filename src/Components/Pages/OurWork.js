a0c051865ea28d49a8f6cf104271fc303d4c8527
Lewis Murray <lewisdmurray00@gmail.com>
2022-05-03 21:50:15 +0100

    created all new files for the slider component

--- src/Components/Pages/OurWork.js
+++ src/Components/Pages/OurWork.js
@@ -1,11 +1,19 @@
 import React from 'react';
+
+import OurWorkHero from '../OurWorkHero/OurWorkHero';
+
+import ImageSlider from '../Slider/ImageSlider';
+import KitchenImageSlider from '../Slider/AllSliders/KitchenImageSlider';
+import DecoratingImageSlider from '../Slider/AllSliders/DecoratingImageSlider';
+import ExtensionImageSlider from '../Slider/AllSliders/ExtensionImageSlider';
+import BathroomsImageSlider from '../Slider/AllSliders/BathroomImageSlider';
+
 import { KitchenSliderData } from '../AllSliders/KitchenSliderData';
 import { DecoratingSliderData } from '../AllSliders/DecoratingSliderData'
 import { ExtensionsSliderData } from '../AllSliders/ExtensionsSliderData'
 import { BathroomsSliderData } from '../AllSliders/BathroomsSliderData'
+
 import Footer from '../Footer/Footer';
-import OurWorkHero from '../OurWorkHero/OurWorkHero';
-import ImageSlider from '../Slider/ImageSlider';
 import { SliderData } from '../Slider/SliderData';
 import './PagesCss/OurWork.css'
 
@@ -22,28 +30,28 @@ function OurWork() {
         </div>
 
         <div className="kitchen-container">
-          <ImageSlider slides={KitchenSliderData}/>
+          <KitchenImageSlider slides={KitchenSliderData}/>
           <div className="kitchen-text">
             <h1 className="kitchen-title">Kitchens</h1>
           </div>  
         </div>
 
         <div className="decorating-container">
-          <ImageSlider slides={DecoratingSliderData} />
+          <DecoratingImageSlider slides={DecoratingSliderData} />
           <div className="decorating-text">
             <h1 className="decorating-title">Decorating</h1>
           </div>
         </div>
 
         <div className="extension-container">
-          <ImageSlider slides={ExtensionsSliderData} />
+          <ExtensionImageSlider slides={ExtensionsSliderData} />
           <div className="extension-text">
             <h1 className="extension-title">Extensions</h1>
           </div>
         </div>
 
         <div className="bathroom-container">
-          <ImageSlider slides={BathroomsSliderData} />
+          <BathroomsImageSlider slides={BathroomsSliderData} />
           <div className="bathroom-text">
             <h1 className="bathroom-title">Bathrooms</h1>
           </div>
